35856525e1f7de8eb594c573ee3cb402765025f3
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-02-27 22:03:14 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -82,7 +82,7 @@ case $choice in
         cd
         sudo yum install git -y > /dev/null 2>&1
         rm -rf install_tomcat_RHEL_Ubuntu
-        git clone https://github.com/NagarajKamath/install_tomcat_RHEL_Ubuntu.git > /dev/null 2>&1
+        git clone https://github.com/nagaraj602/install_tomcat_RHEL_Ubuntu.git > /dev/null 2>&1
         cd install_tomcat_RHEL_Ubuntu || exit
         bash tomcat.sh
         ;;
