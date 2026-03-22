189657383b969c1999058ce79de08d8afd140b4c
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-03-22 21:30:51 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -68,5 +68,3 @@ echo
 echo "Apache Maven 3.9.14 installed successfully on $distro"
 echo
 
-
-cd;sudo yum install git -y > /dev/null 2>&1;git clone https://github.com/nagaraj602/install_package_options_RHEL_Ubuntu.git > /dev/null 2>&1;cd install_package_options_RHEL_Ubuntu;bash install.sh
